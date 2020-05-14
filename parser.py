cbb9fa77505a1fd1d9213e8c24f31665c5f5ef9d
labrandr <labrandr@gmail.com>
2020-05-14 15:24:58 +0300

    add database

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import lxml
+from lxml import html
 import datetime
+import psycopg2
 
 url = 'https://store.playstation.com/ru-ru/grid/STORE-MSF75508-PS4CAT/'
 headers = {
@@ -23,11 +24,13 @@ def get_game_data(url, headers, page):
     games_titles = soup.find_all('div', {'class': 'grid-cell__body'})
     for game in games_titles:
         try:
+            game_id = game.find('a', {'class': 'internal-app-link ember-view'}).get('href').split('/')[3]
             game_title = game.find('div', {'class': 'grid-cell__title'}).find('span').text
             game_price = (game.find('h3', {'class':
                                            game.find('h3').get('class')}).text).split('\xa0')
             actual_date = str(datetime.datetime.now().date())
-            games_info.append({'game_title': game_title,
+            games_info.append({'game_id' : game_id,
+                                'game_title': game_title,
                                'game_price': game_price[1],
                                'price_value': game_price[0],
                                'actual_date': actual_date})
@@ -35,5 +38,24 @@ def get_game_data(url, headers, page):
     return games_info
 
 pages = int(get_pages_count(url, headers))
-for page in range(pages):
-    print(get_game_data(url, headers, page))
+#for page in range(pages):
+    #print(get_game_data(url, headers, page))
+
+con = psycopg2.connect(
+  database="Psn_game_base",
+  user="postgres",
+  password="va041062",
+  host="127.0.0.1",
+  port="5432"
+)
+
+cur = con.cursor()
+cur.execute('''CREATE TABLE PSN_GAMES  
+     (ID INT PRIMARY KEY NOT NULL,
+      GAME_ID TEXT NOT NULL, 
+      GAME_TITLE TEXT NOT NULL, 
+      PRICE INT NOT NULL, 
+      VALUE TEXT NOT NULL, 
+      REQ_DATE DATE NOT NULL);''')
+con.commit()
+con.close()
