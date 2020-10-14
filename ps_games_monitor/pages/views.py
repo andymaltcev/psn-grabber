74255100a13645f6fc23f53981a8792c70f306ea
labrandr <labrandr@gmail.com>
2020-10-14 21:01:14 +0300

    replace for i in range() by item in page_info

--- ps_games_monitor/pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def starting_view(request):
-    return render(request,'pages/index.html')
-
-
-# Create your views here.
